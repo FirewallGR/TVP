a2b26a2398254f3424564056df5d42f6a9573c8b
FireWallGR <qwert.ting@bk.ru>
2022-05-29 01:13:09 +0300

    added task1

---
+++ Task1TVP/Task1TVP/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace Task1TVP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.actionLabel = new System.Windows.Forms.Label();
+            this.keyLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Key = new System.Windows.Forms.TextBox();
+            this.TextLabel = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonStyleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ActionBox = new System.Windows.Forms.ComboBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // actionLabel
+            // 
+            this.actionLabel.AutoSize = true;
+            this.actionLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.actionLabel.Location = new System.Drawing.Point(39, 71);
+            this.actionLabel.Name = "actionLabel";
+            this.actionLabel.Size = new System.Drawing.Size(52, 19);
+            this.actionLabel.TabIndex = 0;
+            this.actionLabel.Text = "Action:";
+            // 
+            // keyLabel
+            // 
+            this.keyLabel.AutoSize = true;
+            this.keyLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.keyLabel.Location = new System.Drawing.Point(39, 130);
+            this.keyLabel.Name = "keyLabel";
+            this.keyLabel.Size = new System.Drawing.Size(38, 19);
+            this.keyLabel.TabIndex = 1;
+            this.keyLabel.Text = "Key:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Release";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(293, 93);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(241, 129);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Key
+            // 
+            this.Key.Location = new System.Drawing.Point(43, 161);
+            this.Key.Name = "Key";
+            this.Key.Size = new System.Drawing.Size(48, 20);
+            this.Key.TabIndex = 4;
+            // 
+            // TextLabel
+            // 
+            this.TextLabel.AutoSize = true;
+            this.TextLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextLabel.Location = new System.Drawing.Point(289, 71);
+            this.TextLabel.Name = "TextLabel";
+            this.TextLabel.Size = new System.Drawing.Size(38, 19);
+            this.TextLabel.TabIndex = 5;
+            this.TextLabel.Text = "Text:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.optionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(546, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backgroundColorToolStripMenuItem,
+            this.buttonStyleToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // backgroundColorToolStripMenuItem
+            // 
+            this.backgroundColorToolStripMenuItem.Name = "backgroundColorToolStripMenuItem";
+            this.backgroundColorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.backgroundColorToolStripMenuItem.Text = "Background color";
+            this.backgroundColorToolStripMenuItem.Click += new System.EventHandler(this.backgroundColorToolStripMenuItem_Click);
+            // 
+            // buttonStyleToolStripMenuItem
+            // 
+            this.buttonStyleToolStripMenuItem.Name = "buttonStyleToolStripMenuItem";
+            this.buttonStyleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.buttonStyleToolStripMenuItem.Text = "Button style";
+            this.buttonStyleToolStripMenuItem.Click += new System.EventHandler(this.buttonStyleToolStripMenuItem_Click);
+            // 
+            // ActionBox
+            // 
+            this.ActionBox.FormattingEnabled = true;
+            this.ActionBox.Items.AddRange(new object[] {
+            "Encrypt",
+            "Decrypt"});
+            this.ActionBox.Location = new System.Drawing.Point(43, 93);
+            this.ActionBox.Name = "ActionBox";
+            this.ActionBox.Size = new System.Drawing.Size(75, 21);
+            this.ActionBox.TabIndex = 7;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 308);
+            this.Controls.Add(this.ActionBox);
+            this.Controls.Add(this.TextLabel);
+            this.Controls.Add(this.Key);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.keyLabel);
+            this.Controls.Add(this.actionLabel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label actionLabel;
+        private System.Windows.Forms.Label keyLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox Key;
+        private System.Windows.Forms.Label TextLabel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backgroundColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buttonStyleToolStripMenuItem;
+        private System.Windows.Forms.ComboBox ActionBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Task1TVP/Task1TVP/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Forms;
+
+namespace Task1TVP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ActionBox.SelectedItem == null)
+            {
+                MessageBox.Show("Action can not be nothing");
+                return;
+            }
+            if (Key.Text.Equals(""))
+            {
+                MessageBox.Show("Key value can not be null");
+                return;
+            }
+            
+            String text = richTextBox1.Text;
+            char[] str = new char[text.Length];
+            char temp;
+            int mod;
+            int div;
+            int key;
+            try
+            {
+                key = Convert.ToInt32(Key.Text);
+            } catch (System.FormatException ex)
+            {
+                MessageBox.Show("Key value has to be a number", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+           
+            Boolean flag = false;
+            if (ActionBox.SelectedItem.ToString().Equals("Encrypt"))
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (text[i] >=65 && text[i] <= 90 || text[i] >= 97 && text[i] <= 122)
+                    {
+                        flag = true;
+                        temp =(char) (text[i] + key);
+                        div = (int)(temp / 123);
+                        mod = (int)(temp % 123);
+                        if (div >= 1)
+                        {
+                            if (mod >= 91 && mod <= 96) str[i] = (char)(65 + 7 + mod);
+                            else str[i] = (char)(65 + mod);
+                        } else
+                        {
+                            if (mod >= 91 && mod <= 96) str[i] = (char)(7 + mod);
+                            else str[i] = (char)(mod);
+                        }
+                    } else
+                    {
+                        str[i] = text[i];
+                    }
+
+                  //  str[i] =(char) (text[i] + Convert.ToInt32(Key.Text));
+                }
+            } else
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (text[i] >= 65 && text[i] <= 122)
+                    {
+                        flag = true;
+                        temp = (char)(text[i] - key);
+                        div = (int)(temp / 123);
+                        mod = (int)(temp % 123);
+                        if (mod < 65)
+                        {
+                            if ((123 - 65 - temp) >= 91 && (123-65-temp) <=96) str[i] = (char)(123 - 65 - 7 - temp);
+                            else str[i] = (char)(123 - (65 - temp));
+                        }
+                        else
+                        {
+                            if (temp >= 91 && temp <= 96) str[i] = (char)(temp - 7);
+                            else str[i] = (char)(temp);
+                        }
+                    }
+                    else
+                    {
+                        str[i] = text[i];
+                    }
+
+                    //  str[i] =(char) (text[i] + Convert.ToInt32(Key.Text));
+                }
+                
+            }
+            if (flag == true)
+            {
+                text = "";
+                for (int i = 0; i < str.Length; i++)
+                {
+                    text += str[i];
+                }
+                richTextBox1.Text = text;
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filename = openFileDialog1.FileName;
+                string text = File.ReadAllText(filename);
+                richTextBox1.Text = text;
+            } 
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = saveFileDialog1.FileName;
+                File.WriteAllText(path, richTextBox1.Text);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBoxButtons messageBoxButtons = MessageBoxButtons.YesNo;
+            DialogResult result;
+            result = MessageBox.Show("Do you want exit the application?", " ", messageBoxButtons);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void backgroundColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Color[] colors = { Color.Aqua, Color.CadetBlue, Color.BlueViolet };
+            Random random = new Random();
+            int num = random.Next(0, 3);
+            this.BackColor = colors[num];
+        }
+
+        private void buttonStyleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            FlatStyle[] styles = { FlatStyle.Flat, FlatStyle.Popup, FlatStyle.Standard, FlatStyle.System };
+            int i = rand.Next(0, 5);
+            button1.FlatStyle = styles[i];
+        }
+    }
+}
