f8c40e7eec631cc9ab0d3526c663507a18a315c2
FireWallGR <qwert.ting@bk.ru>
2022-06-04 17:54:21 +0300

    Switched to one object

--- Task2/Clocks.cs
+++ Task2/Clocks.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Task2
 {
@@ -13,23 +14,119 @@ namespace Task2
         Graphics graphics;
         Size clockSize;
         public Clock Hours, Mins, Secs;
-
+        CheckBox checkBox1;
+        Timer timer1;
+        int width = 360;
+        int height = 120;
         int offsetX, offsetY, hX, mX, sX, Y, sizeX, sizeY;
-        public Clocks(Point p1, Point p2, Graphics graphics) 
+
+
+        public Clocks(int x, int y, Graphics graphics, Form1 form) 
         {
             this.graphics = graphics;
-            Background = new Rectangle(p1.X, p1.Y, p2.X, p2.Y);
-            offsetX = p1.X;
-            offsetY = p1.Y;
-            sizeY = Background.Height;
+            Background = new Rectangle(x, y, x + width, y + height);
+            offsetX = x;
+            offsetY = y;
+            sizeY = height;
             sizeX = sizeY;
             hX = offsetX;
             mX = offsetX + sizeX;
-            sX = offsetX + Background.Width - sizeX;
+            sX = offsetX + width - sizeX;
             Y = offsetY;
             clockSize = new Size(sizeX,sizeY);
+
+            checkBox1 = new CheckBox();
+            checkBox1.Left = Background.X + Background.Width + 50;
+            checkBox1.Top = Background.Y;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            checkBox1.Parent = form;
+            checkBox1.Visible = true;
+            checkBox1.Text = "Start / Stop";
+
+            form.MouseWheel += this_MouseWheel;
+
+            timer1 = new Timer();
+            timer1.Tick += timer1_Tick;
+            timer1.Interval = 1000;
+
+
             Paint();
         }
+
+        private void this_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (checkBox1.Checked) return;
+            if (Hours.crossed(e))
+            {
+                if (e.Delta > 0)
+                {
+                    Hours.Up();
+                }
+                else
+                {
+                    Hours.Down();
+                }
+
+            }
+            else if (Mins.crossed(e))
+            {
+                if (e.Delta > 0)
+                {
+                    Mins.Up();
+                }
+                else
+                {
+                   Mins.Down();
+                }
+            }
+            else if (Secs.crossed(e))
+            {
+                if (e.Delta > 0)
+                {
+                    Secs.Up();
+                }
+                else
+                {
+                    Secs.Down();
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) timer1.Start();
+            else timer1.Stop();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Hours.time == 0 && Mins.time == 0 && Secs.time == 0)
+            {
+                timer1.Stop();
+                checkBox1.Checked = false;
+                MessageBox.Show("Time out");
+                return;
+            }
+            if (Secs.time < 0)
+            {
+                Secs.time = 59;
+                Mins.time -= 1;
+                if (Mins.time < 0)
+                {
+                    Mins.time = 59;
+                    Hours.time -= 1;
+                    Hours.repaintUp();
+                }
+                Mins.repaintUp();
+            }
+            else
+            {
+                Secs.time -= 1;
+                Secs.repaintUp();
+            }
+        }
+
         private void Paint()
         {
             graphics.FillRectangle(new SolidBrush(Color.FromArgb(31, 117, 255)), Background);
--- Task2/Form1.Designer.cs
+++ Task2/Form1.Designer.cs
@@ -29,46 +29,22 @@ namespace Task2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
-            // 
-            // timer1
-            // 
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.checkBox1.Location = new System.Drawing.Point(1094, 291);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(153, 35);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "Start/Stop";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 681);
-            this.Controls.Add(this.checkBox1);
             this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
